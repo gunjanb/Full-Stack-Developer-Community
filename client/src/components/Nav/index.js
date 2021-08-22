5853ba072dc29b0c2b0c03bb8c8d3bc1aac31550
Gunjan Bharagava <gunjanb2006@gmail.com>
2021-08-22 13:38:00 -0700

    just checking for color combination will update in navbar.css

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -30,7 +30,7 @@ function Nav() {
   }
 
   return (
-    <header className="flex-row px-1">
+    <header className="flex-row px-1" style={{ backgroundColor: "#415d43" }}>
       <h1>
         <Link to="/">
           <span role="img" aria-label="shopping bag"></span>
