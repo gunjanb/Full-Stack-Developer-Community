34c63199e8f9d67d007f96b64b66a46d81e48874
Rex Yu <yu19910513@gmail.com>
2021-08-22 17:55:13 -0700

    seeds

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/full_stack_developer_community', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
