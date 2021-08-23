0cc409256d9ec81b683143f94318b14094bcac21
Donna <donnastjames@comcast.net>
2021-08-22 22:52:43 -0700

    Updated styling on Signup.js

--- client/src/pages/Signup/Signup.js
+++ client/src/pages/Signup/Signup.js
@@ -1,21 +1,23 @@
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
 import { Link } from "react-router-dom";
 import { useMutation } from "@apollo/client";
 import Auth from "../../utils/auth";
 import { ADD_USER } from "../../utils/mutations";
+import { Container, Card } from "react-bootstrap";
+import './Signup.css';
+
 
 function Signup(props) {
-  const [formState, setFormState] = useState({ email: "", password: "" });
+  const [formState, setFormState] = useState({ username: "", email: "", password: "" });
   const [addUser] = useMutation(ADD_USER);
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
     const mutationResponse = await addUser({
       variables: {
+        username: formState.username,
         email: formState.email,
         password: formState.password,
-        firstName: formState.firstName,
-        lastName: formState.lastName,
       },
     });
     const token = mutationResponse.data.addUser.token;
@@ -31,56 +33,60 @@ function Signup(props) {
   };
 
   return (
-    <div className="container my-1">
-      <Link to="/login">← Go to Login</Link>
+    <Container fluid>
+      <Card>
+        <div
+          style={{
+            position: 'fixed'
+          }}>
+        <Link to="/login">← Go to Login</Link>
 
-      <h2>Signup</h2>
-      <form onSubmit={handleFormSubmit}>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="firstName">First Name:</label>
-          <input
-            placeholder="First"
-            name="firstName"
-            type="firstName"
-            id="firstName"
-            onChange={handleChange}
-          />
+          <h2>Signup</h2>
+          <form onSubmit={handleFormSubmit}>
+            <label htmlFor="username">
+              Username
+              <br />
+            <input
+              placeholder="Username"
+              name="username"
+              type="text"
+              id="username"
+              onChange={handleChange}
+            />
+            </label>
+            <br />
+            <label htmlFor="email">
+              Email
+              <br />
+            <input
+              placeholder="youremail@email.com"
+              name="email"
+              type="email"
+              id="email"
+              onChange={handleChange}
+            />
+            </label>
+            <br />
+            <label htmlFor="pwd">
+              Password
+              <br />
+            <input
+              placeholder="******"
+              name="password"
+              type="password"
+              id="pwd"
+              onChange={handleChange}
+            />
+            </label>
+            <br />
+            <br />
+            <div className="flex-row flex-end">
+              <button type="submit">Submit</button>
+            </div>
+          </form>
         </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="lastName">Last Name:</label>
-          <input
-            placeholder="Last"
-            name="lastName"
-            type="lastName"
-            id="lastName"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="email">Email:</label>
-          <input
-            placeholder="youremail@test.com"
-            name="email"
-            type="email"
-            id="email"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="pwd">Password:</label>
-          <input
-            placeholder="******"
-            name="password"
-            type="password"
-            id="pwd"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row flex-end">
-          <button type="submit">Submit</button>
-        </div>
-      </form>
-    </div>
+      </Card>
+    </Container>
   );
 }
 
