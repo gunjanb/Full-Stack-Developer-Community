4da2db5bd4025cc335a6ec2ecbc2053202536651
Gunjan Bharagava <gunjanb2006@gmail.com>
2021-08-23 23:56:22 -0700

    updated mutation according to new  schema

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -24,25 +24,50 @@ export const ADD_USER = gql`
   }
 `;
 
-// add photo of user
-export const UPLOAD_PHOTO = gql`
-  mutation uploadPhoto($file: Upload!) {
-    uploadPhoto(file: $file) {
+// add photo of user old
+// export const UPLOAD_PHOTO = gql`
+//   mutation uploadPhoto($file: Upload!) {
+//     uploadPhoto(file: $file) {
+//       _id
+//       username
+//       email
+//       imgUrl
+//       aboutme
+//       contactme
+//       tech {
+//         _id
+//         name
+//       }
+//       resource {
+//         _id
+//         title
+//         videoUrl
+//         shortdescription
+//       }
+//     }
+//   }
+// `;
+
+// new
+export const UPDATE_PHOTO = gql`
+  mutation updateUser($profilePic: file) {
+    updateUser(profilePic: $profilePic) {
       _id
       username
       email
-      imgUrl
-      aboutme
-      contactme
-      tech {
-        _id
-        name
-      }
-      resource {
+      aboutMe
+      profilePic
+      contactInfo
+      posts {
         _id
         title
-        videoUrl
-        shortdescription
+        content
+        video
+        video_title
+        tech {
+          _id
+          name
+        }
       }
     }
   }
@@ -90,10 +115,59 @@ export const UPDATE_TECH = gql`
   }
 `;
 
-// update  aboutme
-export const UPDATE_USER_ABOUT_ME = gql`
-  mutation updateUserAboutMe($aboutme: String!) {
-    updateUserAboutMe(aboutme: $aboutme) {
+// update  aboutme  OLD ONE
+// export const UPDATE_USER_ABOUT_ME = gql`
+//   mutation updateUserAboutMe($aboutme: String!) {
+//     updateUserAboutMe(aboutme: $aboutme) {
+//       _id
+//       username
+//       email
+//       imgUrl
+//       aboutme
+//       contactme
+//       tech {
+//         _id
+//         name
+//       }
+//       resource {
+//         _id
+//         title
+//         videoUrl
+//         shortdescription
+//       }
+//     }
+//   }
+// `;
+
+//needed to update user about me latest
+export const UPDATE_ABOUT_ME = gql`
+  mutation updateUser($aboutMe: String) {
+    updateUser(aboutMe: $aboutMe) {
+      _id
+      username
+      email
+      aboutMe
+      profilePic
+      contactInfo
+      posts {
+        _id
+        title
+        content
+        video
+        video_title
+        tech {
+          _id
+          name
+        }
+      }
+    }
+  }
+`;
+
+//update  contact info of user creating the resource old
+export const UPDATE_USER_CONTACT = gql`
+  mutation updateUserContact($contactInfo: String!) {
+    updateUserContact(contactInfo: $contactInfo) {
       _id
       username
       email
@@ -114,25 +188,26 @@ export const UPDATE_USER_ABOUT_ME = gql`
   }
 `;
 
-//update  contact info of user creating the resource
-export const UPDATE_USER_CONTACT = gql`
-  mutation updateUserContact($contactInfo: String!) {
-    updateUserContact(contactInfo: $contactInfo) {
+// new mutation for contact info
+export const UPDATE_CONTACT_INFO = gql`
+  mutation updateUser($contactInfo: String) {
+    updateUser(contactInfo: $contactInfo) {
       _id
       username
       email
-      imgUrl
-      aboutme
-      contactme
-      tech {
-        _id
-        name
-      }
-      resource {
+      aboutMe
+      profilePic
+      contactInfo
+      posts {
         _id
         title
-        videoUrl
-        shortdescription
+        content
+        video
+        video_title
+        tech {
+          _id
+          name
+        }
       }
     }
   }
@@ -164,6 +239,45 @@ export const UPDATE_USER_RESOURCE = gql`
         title
         videoUrl
         shortdescription
+      }
+    }
+  }
+`;
+
+// video: String
+// video_title: String;
+// addpost
+export const ADD_POST = gql`
+  mutation addPost(
+    $file: Upload!
+    $title: String!
+    $tech: String!
+    $content: String!
+    $video_title: String!
+  ) {
+    addPost(
+      file: $file
+      title: $title
+      tech: $tech
+      content: $content
+      video_title: $video_title
+    ) {
+      _id
+      username
+      email
+      aboutMe
+      profilePic
+      contactInfo
+      posts {
+        _id
+        title
+        content
+        video
+        video_title
+        tech {
+          _id
+          name
+        }
       }
     }
   }
