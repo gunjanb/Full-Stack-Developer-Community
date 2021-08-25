b2667f8e7b46cef3ef4fbfc533e572048945cf60
Donna <donnastjames@comcast.net>
2021-08-25 00:31:45 -0700

    Added client/reducers.js

---
+++ client/src/utils/reducers/reducers.js
@@ -0,0 +1,76 @@
+import { useReducer } from "react";
+import {
+  UPDATE_CONTRIBUTIONS,
+  ADD_TO_CART,
+  UPDATE_CART_QUANTITY,
+  REMOVE_FROM_CART,
+  ADD_MULTIPLE_TO_CART,
+  CLEAR_CART,
+  TOGGLE_CART
+} from "./actions";
+
+export const reducer = (state, action) => {
+  switch (action.type) {
+    case UPDATE_CONTRIBUTIONS:
+      return {
+        ...state,
+        contributions: [...action.contributions],
+      };
+
+    case ADD_TO_CART:
+      return {
+        ...state,
+        cartOpen: true,
+        cart: [...state.cart, action.contribution],
+      };
+
+    case ADD_MULTIPLE_TO_CART:
+      return {
+        ...state,
+        cart: [...state.cart, ...action.contributions],
+      };
+
+    case UPDATE_CART_QUANTITY:
+      return {
+        ...state,
+        cartOpen: true,
+        cart: state.cart.map(contribution => {
+          if (action._id === contribution._id) {
+            contribution.purchaseQuantity = action.purchaseQuantity
+          }
+          return contribution;
+        })
+      };
+
+    case REMOVE_FROM_CART:
+      let newState = state.cart.filter(contribution => {
+        return contribution._id !== action._id;
+      });
+
+      return {
+        ...state,
+        cartOpen: newState.length > 0,
+        cart: newState
+      };
+
+    case CLEAR_CART:
+      return {
+        ...state,
+        cartOpen: false,
+        cart: []
+      };
+
+    case TOGGLE_CART:
+      return {
+        ...state,
+        cartOpen: !state.cartOpen
+      };
+
+    default:
+      return state;
+  }
+};
+
+export function useProductReducer(initialState) {
+  return useReducer(reducer, initialState)
+}
