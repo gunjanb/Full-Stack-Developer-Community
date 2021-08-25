ce3ad57c695c22d06c5b4f4404c109c46619cf8c
Donna <donnastjames@comcast.net>
2021-08-25 00:54:19 -0700

    Updated client/helper.js - changed 'products' to 'contributions' and removed code for 'categories'

--- client/src/utils/helper.js
+++ client/src/utils/helper.js
@@ -1,11 +1,17 @@
+export function pluralize(name, count) {
+  if (count === 1) {
+    return name;
+  }
+  return name + 's';
+}
+
 export function idbPromise(storeName, method, object) {
   return new Promise((resolve, reject) => {
     const request = window.indexedDB.open("full-stack-web-dev-community", 1);
     let db, tx, store;
     request.onupgradeneeded = function (e) {
       const db = request.result;
-      db.createObjectStore("products", { keyPath: "_id" });
-      db.createObjectStore("categories", { keyPath: "_id" });
+      db.createObjectStore("contributions", { keyPath: "_id" });
       db.createObjectStore("cart", { keyPath: "_id" });
     };
 
