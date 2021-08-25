830f8b056dad2a33b928907faff464c1b27be4c9
Tiffany Washke <twashke@gmail.com>
2021-08-24 22:29:38 -0700

    added Donate page until fixed

---
+++ client/src/pages/Donate.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Jumbotron from "../components/Jumbotron";
+
+const Donate = () => {
+  return (
+    <div className="container">
+      <Jumbotron>
+        <h1>Donate</h1>
+        <p>Content Here</p>
+      </Jumbotron>
+    </div>
+  );
+};
+
+export default Donate;
