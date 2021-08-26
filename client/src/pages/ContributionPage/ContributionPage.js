bdb823c95fb8edb8c44264718d2b5804558ec928
Donna <donnastjames@comcast.net>
2021-08-26 14:32:18 -0700

    Contribution images are now showing properly

--- client/src/pages/ContributionPage/ContributionPage.js
+++ client/src/pages/ContributionPage/ContributionPage.js
@@ -1,13 +1,17 @@
 import React from "react";
 import ProductList from "../../components/ProductList";
 import Cart from "../../components/Cart";
+import Container from 'react-bootstrap/Container';
+import './index.css';
 
 const ContributionPage = () => {
   return (
-    <div className="container">
-      <ProductList />
-      <Cart />
-    </div>
+    <Container>
+      <div className="container">
+        <ProductList />
+        <Cart />
+      </div>
+    </Container>
   );
 };
 
