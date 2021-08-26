9bc2b5876afafb6c3574fc622e3ddd457c1bd138
Gunjan Bharagava <gunjanb2006@gmail.com>
2021-08-26 02:43:19 -0700

    started  profilepage

--- client/src/pages/UserProfile/UserProfile.js
+++ client/src/pages/UserProfile/UserProfile.js
@@ -1,15 +1,54 @@
 import React from "react";
-import Jumbotron from "../../components/Jumbotron";
+// import Jumbotron from "../../components/Jumbotron";
+
+// const UserProfile = () => {
+//   return (
+//     <div className="container">
+//       <Jumbotron>
+//         <h1>User Profile</h1>
+//         <p>Content Here</p>
+//       </Jumbotron>
+//     </div>
+//   );
+// };
+
+// export default UserProfile;
+import { useState, useEffect } from "react";
+import { useQuery } from "@apollo/client";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams, Link } from "react-router-dom";
+import { QUERY_USERS } from "../../utils/queries";
+import { UPDATE_USERS } from "../../utils/actions";
+import { idbPromise } from "../../utils/helper";
+import { Row, Col, Spinner, Card } from "react-bootstrap";
+import "./UserProfile.css";
 
 const UserProfile = () => {
-  return (
-    <div className="container">
-      <Jumbotron>
-        <h1>User Profile</h1>
-        <p>Content Here</p>
-      </Jumbotron>
-    </div>
-  );
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state);
+  const { users } = state;
+  const { profileId } = useParams();
+  const { loading, data } = useQuery(QUERY_USERS);
+  const [currentUser, setCurrentUser] = useState({});
+
+  useEffect(() => {
+    if (users.length) {
+      console.log(data);
+      setCurrentUser(data.users.find((user) => user._id === profileId));
+    } else if (data) {
+      dispatch({
+        type: UPDATE_USERS,
+        payload: data.users,
+      });
+      data.users.forEach((user) => {
+        idbPromise("users", "put", user);
+      });
+    } else if (!loading) {
+      idbPromise("users", "get").then((users) => {
+        dispatch({ type: UPDATE_USERS, payload: users });
+      });
+    }
+  }, [data, profileId, dispatch]);
 };
 
 export default UserProfile;
